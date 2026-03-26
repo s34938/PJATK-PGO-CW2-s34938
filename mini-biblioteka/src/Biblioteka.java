8c5c63a43a59ae71e386bd6f039b485aea29dc43
s34938 <s34938@pjwstk.edu.pl>
2026-03-26 15:19:36 +0100

    Utworzenie konstruktora

--- mini-biblioteka/src/Biblioteka.java
+++ mini-biblioteka/src/Biblioteka.java
@@ -2,11 +2,26 @@ public class Biblioteka {
     private Ksiazka[] ksiazki;
     private int liczbaKsiazek;
 
-    public Biblioteka(int pojemnosc) {
-        // inicjalizacja tablicy
+    public Biblioteka(int pojemnosc)
+    {
+        this.ksiazki=new Ksiazka[pojemnosc];
+        this.liczbaKsiazek=0;
     }
 
     // public void dodajKsiazke(Ksiazka ksiazka)
     // { // logika dodawania do tablicy }
     
 }
+
+
+pole Ksiazka[] ksiazki oraz pole int liczbaKsiazek
+
+· konstruktor przyjmujący maksymalny rozmiar tablicy
+
+· metoda dodajKsiazke(Ksiazka ksiazka)
+
+· metoda wypiszDostepneKsiazki() wykorzystująca pętlę
+
+· metoda znajdzKsiazkePoTytule(String tytul)
+
+· metoda policzDostepneKsiazki() zwracająca liczbę książek dostępnych
