8f8e1412f8148e839c2b60fb575dc76e27cabe95
s34938 <s34938@pjwstk.edu.pl>
2026-03-26 15:16:51 +0100

    Utworzenie pliku Biblioteka.java

---
+++ mini-biblioteka/src/Biblioteka.java
@@ -0,0 +1,12 @@
+public class Biblioteka {
+    private Ksiazka[] ksiazki;
+    private int liczbaKsiazek;
+
+    public Biblioteka(int pojemnosc) {
+        // inicjalizacja tablicy
+    }
+
+    // public void dodajKsiazke(Ksiazka ksiazka)
+    // { // logika dodawania do tablicy }
+    
+}
