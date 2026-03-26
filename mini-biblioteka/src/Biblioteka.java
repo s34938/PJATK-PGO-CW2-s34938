0cf8fa7e650cd0a8b4633d27ddcd00b05093b2ce
s34938 <s34938@pjwstk.edu.pl>
2026-03-26 15:33:51 +0100

    Dodanie drugiej metody

--- mini-biblioteka/src/Biblioteka.java
+++ mini-biblioteka/src/Biblioteka.java
@@ -31,7 +31,24 @@ public class Biblioteka {
             }
         }
     }
-    
+
+    public void znajdzKsiazkePoTytule(String tytul)
+    {
+        for (int i = 0; i < liczbaKsiazek; i++)
+        {
+            if(ksiazki[i].tytul.equals(tytul))
+            {
+                System.out.println("Znaleziono ksiązkę: " + tytul);
+                ksiazki[i].wypiszInfo();
+                break;
+            } else
+            {
+                System.out.println("Nie znaleziono książki: " + tytuł);
+            }
+        }
+    }
+
+
 }
 
 
@@ -43,6 +60,6 @@ pole Ksiazka[] ksiazki oraz pole int liczbaKsiazek
 
 · metoda wypiszDostepneKsiazki() wykorzystująca pętlę
 
-· metoda znajdzKsiazkePoTytule(String tytul)
+· metoda
 
 · metoda policzDostepneKsiazki() zwracająca liczbę książek dostępnych
