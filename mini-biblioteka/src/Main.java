e56ce9ded9455dcdccf24b4f0d1fc9b02f4c9802
s34938 <s34938@pjwstk.edu.pl>
2026-03-26 16:36:22 +0100

    Dodanie ksiazek do biblioteki

--- mini-biblioteka/src/Main.java
+++ mini-biblioteka/src/Main.java
@@ -4,6 +4,12 @@ public class Main
     {
         Biblioteka biblioteka = new Biblioteka(10);
         Ksiazka k1 = new Ksiazka("Potop", "Sienkiewicz", 800, true);
-        //Ksiazka k2 = new Ksiazka
+        Ksiazka k2 = new Ksiazka("Mały Książe", "de Saint-Exupéry", 90, true);
+        Ksiazka k3 = new Ksiazka("Hobbit", "Tolkien", 300, true);
+
+        biblioteka.dodajKsiazke(k1);
+        biblioteka.dodajKsiazke(k2);
+        biblioteka.dodajKsiazke(k3);
+
     }
 }
