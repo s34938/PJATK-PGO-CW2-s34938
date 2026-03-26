c65d14f1964983d2621f24a09e058f0ab5feee84
s34938 <s34938@pjwstk.edu.pl>
2026-03-26 14:29:06 +0100

    Ustawienie konstruktora

---
+++ mini-biblioteka/src/Ksiazka.java
@@ -0,0 +1,14 @@
+public class Ksiazka {
+    private String tytul;
+    private String autor;
+    private int liczbaStron;
+    private boolean dostepna;
+
+    public Ksiazka(String tytul, String autor, int liczbaStron, boolean dostepna) {
+        this.tytul=tytul;
+        this.autor=autor;
+        this.liczbaStron=liczbaStron;
+        this.dostepna=dostepna;
+    }
+
+}
